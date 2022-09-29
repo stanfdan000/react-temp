3a6ca2f88fa993aa4b619406db71757800945c0f
sirdan <stanford303@gmail.com>
2022-09-29 12:50:01 -0700

    removed console.logs

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,3 +1,30 @@
+// import styles from './Dashboard.css';
+import { 
+  InputController,
+  TextAreaControl,
+  SelectControl,
+  CheckBoxController,
+  FormButton,
+} from '../Forms/FormController.jsx';
+
+
 export default function Users() {
-  return <div>Users</div>;
+  return (
+    <div>
+      <form>
+        <InputController
+          label="customer"
+          name="name"
+          placeholder="Your name"/>
+
+        <InputController
+          label="Time due"
+          required
+          type="date"
+          name="date"
+          placeholder="Pick a date"/>
+      </form>
+    </div>
+
+  );
 }
